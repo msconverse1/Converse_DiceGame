3503997ba2e680f21fe8a9abb82b2cf19ac76f26
msconverse1 <msconverse@fullsail.edu>
2018-12-11 10:25:57 -0600

    Created AI functionallity + AI Turn after Player + Output and update HP value + Limit AI to set of Heals

--- DiceGame.js
+++ DiceGame.js
@@ -31,44 +31,88 @@
 //  	}
 // }
 
-//generate a random dice number
+//generate a random dice number for the Player
 function rollDice(input){
 	let result;
 	if (input == 20) {
 	 result = Math.floor(Math.random()*input+1);
-	let resultCP = Math.floor(Math.random()*input+1);
 	document.getElementById('display').innerHTML = result;
-	document.getElementById('display3').innerHTML = resultCP;
-
-	HitorMissed(result,resultCP);
+	HitorMissed(result,0);
 	}
-	if (input == 10 || input ==8 || input ==6 || input ==4) {
+	else if (input == 10 || input ==8 || input ==6 || input ==4) {
 		result = Math.floor(Math.random()*input+1);
-		dealDamage(input,result);
+		dealDamage(input,result,0);
 	}
+	else if (input == 12){
+		if(PHP <120){
+		result = Math.floor(Math.random()*input+1);
+		document.getElementById('display2').innerHTML = "Healed For:";
+		Heal(result,0);
 
+		}
+		else{
+			document.getElementById('display2').innerHTML = "Can't Heal";
+		}
+	}
+	CPTrun();
+}
+let healLimit=0;
+//CP rolls Dice
+function CPTrun(){
+	
+	var diceArray = [12,20,12,20,20,20,20,20,20,20,20,20];
+	var randomDice = diceArray[Math.floor(Math.random()*diceArray.length)];
+	let result=0;
+	if (randomDice == 20) {
+		result = Math.floor(Math.random()*randomDice+1);
+		document.getElementById('display3').innerHTML = result;
+	HitorMissed(result,1);
+	}
+	else if (randomDice == 12){
+		if(CHP <120 && healLimit < 5){
+		result = Math.floor(Math.random()*randomDice+1);
+		document.getElementById('display3').innerHTML = result;
+		document.getElementById('display4').innerHTML = "Healed for:";
+		healLimit++;
+		Heal(result,1)}
+		else{
+			document.getElementById('display4').innerHTML = "Can't Heal";
+		}
+	}
+}
+
+function CPDamageDice(){
+	var diceArray = [4,6,8,10];
+	var randomDice = diceArray[Math.floor(Math.random()*diceArray.length)];
+	let result=Math.floor(Math.random()*randomDice+1);
+	dealDamage(randomDice,result,1)
 }
 // check if hit or missed
-function HitorMissed(attack,attackCP){
+function HitorMissed(attack,turn){
 	let AICrAC = "14";
 	let word = "";
 	let PAC = "15";
-	if (attack >= AICrAC) {
-		 word = "you hit"
+	if (turn ==0) {
+		if (attack >= AICrAC && turn ==0) {
+			 word = "you hit"
+			document.getElementById('display2').innerHTML = word;
+			startDamage();
+		}
+		else {
+			word = "you Missed";
 		document.getElementById('display2').innerHTML = word;
-		startDamage();
-	}
-	else {
-		word = "you Missed";
-	document.getElementById('display2').innerHTML = word;
-	}
-	if (attackCP >= PAC) {
-		word = "you hit"
-		document.getElementById('display4').innerHTML = word;
+		}
 	}
 	else{
-		word = "you Missed";
-	document.getElementById('display4').innerHTML = word;
+		if (attack >= PAC) {
+			word = "you hit"
+			document.getElementById('display4').innerHTML = word;
+			CPDamageDice();
+		}
+		else{
+			word = "you Missed";
+		document.getElementById('display4').innerHTML = word;
+		}
 	}
 }
 //Toggle to Damage Dice
@@ -81,44 +125,95 @@ function startDamage(){
 	document.getElementById("D20").style.display = "none";
 }
 //Create Damage
-function dealDamage(dice,result)
+function dealDamage(dice,result,turn)
 {
-	if (dice == 10) {
-		document.getElementById('display2').innerHTML = "Dagger Hit!";
-		Health(result);
-	}
-		if (dice == 8) {
-		document.getElementById('display2').innerHTML = "Magic Hit!";
-		Health(result);
+	if (turn ==0) {
+		if (dice == 10) {
+			document.getElementById('display').innerHTML = result;
+			document.getElementById('display2').innerHTML = "Dagger Hit!";
+			Health(result,0);
+		}
+		else if (dice == 8) {
+			document.getElementById('display').innerHTML = result;
+			document.getElementById('display2').innerHTML = "Magic Hit!";
+			Health(result,0);
 
+		}
+			else if (dice == 6) {
+				document.getElementById('display').innerHTML = result;
+			document.getElementById('display2').innerHTML = "Rock Hit!";
+			Health(result,0);
+		}
+			else if (dice == 4) {
+				document.getElementById('display').innerHTML = result;
+			document.getElementById('display2').innerHTML = "Life Steal!";
+			Health(result,0);
+			Heal(result,0);
+		}
 	}
-		if (dice == 6) {
-		document.getElementById('display2').innerHTML = "Rock Hit!";
-		Health(result);
-	}
-			if (dice == 4) {
-		document.getElementById('display2').innerHTML = "Life Steal!";
-		Health(result);
-		Heal(result);
+	else{
+				if (dice == 10) {
+					document.getElementById('display3').innerHTML = result;
+			document.getElementById('display4').innerHTML = "Dagger Hit!";
+			Health(result,1);
+		}
+			else if (dice == 8) {
+				document.getElementById('display3').innerHTML = result;
+			document.getElementById('display4').innerHTML = "Magic Hit!";
+			Health(result,1);
+
+		}
+			else if (dice == 6) {
+				document.getElementById('display3').innerHTML = result;
+			document.getElementById('display4').innerHTML = "Rock Hit!";
+			Health(result,1);
+		}
+			else if (dice == 4) {
+				document.getElementById('display3').innerHTML = result;
+			document.getElementById('display4').innerHTML = "Life Steal!";
+			Health(result,1);
+			Heal(result,1);
+		}
 	}
 }
 	let PHP =0;
 	let CHP =0;
-function Health(damage)
+//Update Display 5&6 based on turn
+function Health(damage,turn)
 {
-	CHP -=damage;
- document.getElementById('display6').innerHTML = "HP: "+ CHP;
- 	 document.getElementById("D4").style.display = "none";
-	 document.getElementById("D6").style.display = "none";
-	 document.getElementById("D8").style.display = "none";
-	 document.getElementById("D10").style.display = "none";
-	 document.getElementById("D12").style.display = "inline";
-	 document.getElementById("D20").style.display = "inline";
-	 GameOver(PHP,CHP)
+	if (turn == 0) {
+		CHP -=damage;
+	 	document.getElementById('display6').innerHTML = "HP: "+ CHP;
+	 	 document.getElementById("D4").style.display = "none";
+		 document.getElementById("D6").style.display = "none";
+		 document.getElementById("D8").style.display = "none";
+		 document.getElementById("D10").style.display = "none";
+		 document.getElementById("D12").style.display = "inline";
+		 document.getElementById("D20").style.display = "inline";
+		 GameOver(PHP,CHP)
+	}
+	else{
+		PHP -=damage;
+	 	document.getElementById('display5').innerHTML = "HP: "+ PHP;
+	 	GameOver(PHP,CHP)
+	}
 }
-function Heal(dice){
-	PHP +=dice;
-	 document.getElementById('display5').innerHTML = "HP: "+ PHP;
+// Heals off d12 or d4 attack
+function Heal(dice,turn){
+	if (turn == 0) {
+		PHP +=dice;
+		if(PHP > 120){
+			PHP =120;
+		}
+	 	document.getElementById('display5').innerHTML = "HP: "+ PHP;
+	 }
+	 else{
+	 		CHP +=dice;
+	 		if(CHP >120){
+	 			CHP =120;
+	 		}
+	 	document.getElementById('display6').innerHTML = "HP: "+ CHP;
+	 }
 }
 
 // create new game
